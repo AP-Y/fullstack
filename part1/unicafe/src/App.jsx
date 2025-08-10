9d60c79ff48ff3995fe19fc3a66e58e8e684e6cc
apy-gt <aarna.pal-yadav@gt.school>
2025-08-10 15:06:49 -0400

    Completed ex.1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,22 @@ const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const Stat = ({name, num}) => <p>{name} {num}</p>
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const noReviews = total === 0
+
+  return (
+    <div>
+      <Stat name="good" num={good} />
+      <Stat name="neutral" num={neutral} />
+      <Stat name="bad" num={bad} />
+      <Stat name="all" num={total} />
+      <Stat name="average" num={noReviews ? 0 : (good - bad)/total} />
+      <Stat name="positive" num={(noReviews ? 0 : (good/total*100).toString()) + "%"} />
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -18,12 +34,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-      <Stat name="good" num={good} />
-      <Stat name="neutral" num={neutral} />
-      <Stat name="bad" num={bad} />
-      <Stat name="all" num={good + neutral + bad} />
-      <Stat name="average" num={(good - bad)/(good + neutral + bad)} />
-      <Stat name="positive" num={(good/(good + neutral + bad)*100).toString() + "%"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
