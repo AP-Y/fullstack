181104b04a6b098a5a92763a85adeb391de6b8dc
apy-gt <aarna.pal-yadav@gt.school>
2025-08-10 11:27:12 -0400

    Completed ex.1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,20 +34,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header name={course} />
-      <Content name1={part1} num1={exercises1}
-               name2={part2} num2={exercises2}
-               name3={part3} num3={exercises3} />
-      <Total num={exercises1 + exercises2 + exercises3} />
+      <Content name1={part1.name} num1={part1.exercises}
+               name2={part2.name} num2={part2.exercises}
+               name3={part3.name} num3={part3.exercises} />
+      <Total num={part1.exercises + part2.exercises + part3.exercises} />
     </>
   )
 }
