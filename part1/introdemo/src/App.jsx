e00462aa1b1110c0cc2bca11da146620314b5504
apy-gt <aarna.pal-yadav@gt.school>
2025-08-10 13:58:13 -0400

    Completed part1.d introdemo

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const Display = ({value}) => <div>{value}</div>
 
-const Button = ({onClick, display}) => <button onClick={onClick}>{display}</button>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}  </button>
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log("rendering with counter value", counter)
+  const [value, setValue] = useState(10)
 
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter)
-    setCounter(counter + 1)
-  }
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter)
-    setCounter(counter - 1)
-  }
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter)
-    setCounter(0)
+  const setToValue = (newValue) => {
+    console.log("value now", newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} display="plus" />
-      <Button onClick={decreaseByOne} display="minus" />
-      <Button onClick={setToZero} display="zero" />
-    </div>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value+1)} text="increase" />
+   </div>
   )
 }
 
