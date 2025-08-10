01acce3c0da3528d2c2b283a591dee003e8fe184
apy-gt <aarna.pal-yadav@gt.school>
2025-08-10 13:06:34 -0400

    Completed part1.c introdemo

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, display}) => <button onClick={onClick}>{display}</button>
+
 const App = () => {
-  const friends = [ "Peter", "Maya" ]
+  const [ counter, setCounter ] = useState(0)
+  console.log("rendering with counter value", counter)
+
+  const increaseByOne = () => {
+    console.log("increasing, value before", counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log("decreasing, value before", counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log("resetting to zero, value before", counter)
+    setCounter(0)
+  }
 
   return (
-    <>
-      <p>{friends}</p>
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} display="plus" />
+      <Button onClick={decreaseByOne} display="minus" />
+      <Button onClick={setToZero} display="zero" />
+    </div>
   )
 }
 
