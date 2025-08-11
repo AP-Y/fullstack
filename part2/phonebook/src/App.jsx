cf6d4caa7785d488b65837a033b900a7125b24f4
apy-gt <aarna.pal-yadav@gt.school>
2025-08-11 17:47:07 -0400

    Completed ex.2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+  const [newPerson, setNewPerson] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newPerson}))
+  }
+
+  const handlePersonChange = (event) => setNewPerson(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newPerson} onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
