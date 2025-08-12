5029062c9223991c4994e893b5dcf38a78d909ac
apy-gt <aarna.pal-yadav@gt.school>
2025-08-11 20:03:30 -0400

    Completed ex.2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/Header'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Display from './components/Display'
 
-const App = ({initialPersons}) => {
+const App = () => {
   const emptyPerson = {name: '', number: ''}
-  const [persons, setPersons] = useState(initialPersons)
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState(emptyPerson)
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
