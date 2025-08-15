9faace748137d01203e79b4ae3148df66dd7173d
apy-gt <aarna.pal-yadav@gt.school>
2025-08-15 19:47:38 -0400

    Completed ex.2.20

--- part2/countrydata/src/components/Weather.jsx
+++ part2/countrydata/src/components/Weather.jsx
@@ -9,16 +9,37 @@ const Weather = ({country}) => {
     countryData
       .getWeather(lat, lon, import.meta.env.VITE_OPENWEATHER_API)
       .then(w => {
-        console.log(w)
         setWeather(w)
-        console.log(weather)
       })
-  }, [country, weather])
+  }, [country])
+
+  let curTemp
+  let curWindSpeed
+  let curIcon
+  let curAltText
+
+  if (weather) {
+    curTemp = weather.current.temp - 273.15
+    curWindSpeed = weather.current.wind_speed
+    curIcon = `https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`
+    curAltText = weather.current.weather.description
+  } else {
+    curTemp = 0
+    curWindSpeed = 0
+    curIcon = null
+    curAltText = null
+  }
 
   return (
     <div>
       <h2>Weather in {country.capital}</h2>
-
+      {weather
+        ? <><p>Temperature {curTemp.toFixed(2)} Celsius</p>
+            <img src={curIcon} alt={curAltText} />
+            <p>Wind {curWindSpeed.toFixed(1)} m/s</p>
+          </>
+        : ''
+      }
     </div>
   )
 }
